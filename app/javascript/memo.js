cf4eacbaafe92e159093c47a22d188196e1421f6
1209masayuki <xkiddo22@yahoo.co.jp>
2020-08-01 15:18:16 +0900

    メモ投稿の本当の完成

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -2,7 +2,7 @@ function memo() {
   const submit = document.getElementById("submit");
   submit.addEventListener("click", (e) => {
     const formData = new FormData(document.getElementById("form"));
-    const XHR = new XMLHttpRequest():
+    const XHR = new XMLHttpRequest();
     XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
     XHR.send(formData);
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'posts#index'
   # get 'posts/new', to: 'posts#new'
   post 'posts', to: 'posts#create'
-  get 'posts', to: 'posts#index'
   get 'posts/:id', to: 'posts#checked'
+  get 'posts', to: 'posts#index'
 end
